1ab794a3374d4b380abc8f2c0991ab32d1c74a94
khaledElgohary <121522162+khaledElgohary@users.noreply.github.com>
2024-12-27 03:18:37 -0600

    ..

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,19 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import posthog from 'posthog-js'
+
+import { PostHogProvider } from 'posthog-js/react'
+
+posthog.init('phc_iQ9ZdWZL3ajz4F7LlYJ6k3U6IHNS8SbO2xjk6Qb42tA', {
+  api_host:'https://us.i.posthog.com',
+  person_profiles: 'identified_only' 
+})
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <PostHogProvider client={posthog}>
+      <App />
+    </PostHogProvider>
   </StrictMode>,
 )
